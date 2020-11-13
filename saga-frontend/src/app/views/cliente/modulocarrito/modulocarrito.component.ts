2780fe320afd65db0b0b0de7a6e3d514e18855e7
DagAburto <dag.aburtogonzalez@hotmail.com>
2020-11-13 15:36:20 -0600

    Avances de libros desde la vista de usuarios 25/100

--- saga-frontend/src/app/views/cliente/modulocarrito/modulocarrito.component.ts
+++ saga-frontend/src/app/views/cliente/modulocarrito/modulocarrito.component.ts
@@ -21,16 +21,16 @@ export class CarritoComponent implements OnInit {
       this.modulonumero = 1;
       sessionStorage.setItem('mimodulo', this.modulonumero.toString());
     }
-    if (sessionStorage.getItem('mensaje') === '') {
+    /*if (sessionStorage.getItem('mensaje') === '') {
       this.mensaje = 'Hola soy el modulo numero: ' + this.modulonumero.toString();
     } else {
-      this.mensaje = sessionStorage.getItem('mensaje');
-    }
+    }*/
+    this.mensaje = sessionStorage.getItem('mensaje');
+
     if (sessionStorage.getItem('azar') === '1') {
       this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
     } else {
       this.color = sessionStorage.getItem('color');
     }
   }
-
 }
