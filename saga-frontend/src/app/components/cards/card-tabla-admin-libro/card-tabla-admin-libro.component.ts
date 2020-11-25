afa05d5751cf0229caf029ff23a8bc54c3222d7c
Yolanda Bustamante <yolanda.bustamante@est.uca.edu.ni>
2020-11-25 02:03:03 -0600

    Eliminar libro desde interfaz

--- saga-frontend/src/app/components/cards/card-tabla-admin-libro/card-tabla-admin-libro.component.ts
+++ saga-frontend/src/app/components/cards/card-tabla-admin-libro/card-tabla-admin-libro.component.ts
@@ -1,6 +1,7 @@
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component, OnInit, Input } from "@angular/core";
 import { SharedService } from "src/app/shared.service";
+import swal from'sweetalert2';
 
 @Component({
   selector: "app-card-tabla-admin-libro",
@@ -27,6 +28,30 @@ export class CardTablaLibroComponent implements OnInit {
     refreshLibroList(){
       this.service.getLibroList().subscribe(data=>{
         this.LibroList=data;
+      });
+    }
+
+    deleteLibro(id:number){
+      swal.fire({
+        title: '¿Estás seguro?',
+        text: "Si borras este registro podrías alterar otra tabla",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#f44e3c',
+        cancelButtonColor: '#a5a5a5',
+        confirmButtonText: 'Eliminar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.service.deleteLibro(id).subscribe(data=>{
+            this.refreshLibroList();
+          });
+          swal.fire(
+            'Éxito',
+            'El registro fue eliminado',
+            'success'
+          )
+        }
       });
     }
 }
