739c101dbbb97a541753f3c8319b820025e810ac
Marialetsandra Carrazco <marialetsandra.carrazco@est.uca.edu.ni>
2020-11-24 20:00:44 -0600

    Cargar al carrito y validación de existencias listo

--- saga-frontend/src/app/views/cliente/carrito/carrito.component.ts
+++ saga-frontend/src/app/views/cliente/carrito/carrito.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
+import { SharedService } from "src/app/shared.service";
 import swal from'sweetalert2';
 
 @Component({
@@ -9,19 +10,42 @@ import swal from'sweetalert2';
 export class CarritoComponent implements OnInit {
   tokenUser:string;
   tokenRol:number;
+  LibroList:any=[];
+  CarritoList:any=[];
+  DetalleList:any=[];
 
-  constructor(private router:Router) {
+  constructor(private service:SharedService, private router:Router) {
     this.tokenUser = localStorage.getItem('tokenUser');
     this.tokenRol = parseInt(localStorage.getItem('tokenRol'), 10);
+
+    if(localStorage.getItem('carrito')){
+      this.CarritoList = JSON.parse(localStorage.getItem('carrito'));
+    }
   }
 
   ngOnInit() {
     this.autenticacion();
+    this.refreshLibroList();
   }
 
   autenticacion(){
     if(this.tokenRol != 2){
       this.router.navigate(['/error404']);
+    }
+  }
+
+  refreshLibroList(){
+    for(let i of this.CarritoList){
+      this.service.getLibroIndividual(i.IdLibro).subscribe(data=>{
+        this.LibroList = data;
+        var val = {
+          titulo:this.LibroList.titulo,
+          autor:this.LibroList.nombres+ " " + this.LibroList.apellidos,
+          categoria:this.LibroList.nombreCategoria,
+          cantidad:i.Cantidad
+        };
+        this.DetalleList.push(val);
+      });
     }
   }
 
